7a6a1d47f81bcc55178fee6a16700a985efe02fe
Shashikant Dwivedi <shashikant@shashikantdwivedi.com>
2020-07-08 20:23:36 +0530

    Completed Two Screens

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -205,98 +206,147 @@ class _MainScreen extends State<MainScreen> {
           itemCount: allChats.length,
           itemBuilder: (BuildContext context, int index) {
             Map lastActivity = allChats[index]['lastActivity'];
-            return Container(
-              height: 100,
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.all(10),
-              // decoration: BoxDecoration(border: Border.all()),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                      child: Row(children: [
-                    Container(
-                      width: 72,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25)),
-                      child: Image.asset(allChats[index]['image']),
-                    ),
-                    Container(
-                        padding: EdgeInsets.all(8),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              margin: EdgeInsets.symmetric(vertical: 2),
-                              child: Text(
-                                allChats[index]['name'],
-                                style: TextStyle(
-                                    fontFamily: 'Gilroy Bold',
-                                    fontSize: 19,
-                                    color: Color(0xFF131313)),
-                              ),
-                            ),
-                            Container(
-                              width: 160,
-                              margin: EdgeInsets.symmetric(vertical: 2),
-                              child: Text(
-                                lastActivity['message'],
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    fontFamily: 'Gilroy Bold',
-                                    fontSize: 12,
-                                    color: lastActivity['send'] || lastActivity['status'] == -1 
-                                        ? Color(0xFF848484)
-                                        : Color(0xFF2675EC)),
-                              ),
-                            )
-                          ],
-                        )),
-                  ])),
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+            return Slidable(
+              child: InkWell(
+                  onTap: () {},
+                  splashColor: Color(0xff2675EC).withOpacity(0.5),
+                  child: Container(
+                    height: 100,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.all(10),
+                    // decoration: BoxDecoration(border: Border.all()),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Container(
-                            margin: EdgeInsets.symmetric(vertical: 2),
-                            child: Text(
-                              lastActivity['time'],
-                              style: TextStyle(
-                                  fontFamily: 'Gilroy Medium',
-                                  fontSize: 13,
-                                  color: Color(0xFF848484)),
-                            )),
-                        lastActivity['send']
-                            ? (lastActivity['status'] != 3
-                                ? Container(
+                            child: Row(children: [
+                          Container(
+                            width: 82,
+                            padding: EdgeInsets.all(5),
+                            // decoration: BoxDecoration(border: Border.all()),
+                            child: Stack(
+                              children: [
+                                Image.asset(
+                                  allChats[index]['image'],
+                                  width: 72,
+                                ),
+                                allChats[index]['status'] == 1
+                                    ? Positioned(
+                                        bottom: 0,
+                                        right: 0,
+                                        child: Container(
+                                          width: 23,
+                                          height: 23,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(11.5),
+                                              color: Colors.white),
+                                          child: CustomPaint(
+                                              painter: OnlineStatusCircle()),
+                                        ),
+                                      )
+                                    : Container()
+                              ],
+                            ),
+                          ),
+                          Container(
+                              padding: EdgeInsets.all(8),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
                                     margin: EdgeInsets.symmetric(vertical: 2),
-                                    child:
-                                        allSendStatus[lastActivity['status']])
-                                : Container())
-                            : (allChats[index]['unreadMessages'] != 0
-                                ? Container(
-                                    margin: EdgeInsets.symmetric(vertical: 2),
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(15),
-                                        color: Color(0xFF2675EC)),
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 10, vertical: 7),
                                     child: Text(
-                                      allChats[index]['unreadMessages']
-                                          .toString(),
+                                      allChats[index]['name'],
                                       style: TextStyle(
-                                          color: Colors.white,
-                                          fontFamily: 'Gilroy Medium',
-                                          fontSize: 11),
-                                    ))
-                                : Container())
+                                          fontFamily: 'Gilroy Bold',
+                                          fontSize: 19,
+                                          color: Color(0xFF131313)),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 160,
+                                    margin: EdgeInsets.symmetric(vertical: 2),
+                                    child: Text(
+                                      lastActivity['message'],
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                          fontFamily: 'Gilroy Bold',
+                                          fontSize: 12,
+                                          color: lastActivity['send'] ||
+                                                  lastActivity['status'] == -1
+                                              ? Color(0xFF848484)
+                                              : Color(0xFF2675EC)),
+                                    ),
+                                  )
+                                ],
+                              )),
+                        ])),
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                  margin: EdgeInsets.symmetric(vertical: 2),
+                                  child: Text(
+                                    lastActivity['time'],
+                                    style: TextStyle(
+                                        fontFamily: 'Gilroy Medium',
+                                        fontSize: 13,
+                                        color: Color(0xFF848484)),
+                                  )),
+                              lastActivity['send']
+                                  ? (lastActivity['status'] != 3
+                                      ? Container(
+                                          margin:
+                                              EdgeInsets.symmetric(vertical: 2),
+                                          child: allSendStatus[
+                                              lastActivity['status']])
+                                      : Container())
+                                  : (allChats[index]['unreadMessages'] != 0
+                                      ? Container(
+                                          margin:
+                                              EdgeInsets.symmetric(vertical: 2),
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(15),
+                                              color: Color(0xFF2675EC)),
+                                          padding: EdgeInsets.symmetric(
+                                              horizontal: 10, vertical: 7),
+                                          child: Text(
+                                            allChats[index]['unreadMessages']
+                                                .toString(),
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontFamily: 'Gilroy Medium',
+                                                fontSize: 11),
+                                          ))
+                                      : Container())
+                            ],
+                          ),
+                        )
                       ],
                     ),
-                  )
-                ],
-              ),
+                  )),
+              actionPane: SlidableStrechActionPane(),
+              secondaryActions: [
+                SlideAction(
+                    decoration: BoxDecoration(color: Color(0xFF2675EC)),
+                    child: Icon(
+                      Icons.bookmark_border,
+                      color: Colors.white,
+                      size: 22,
+                    )),
+                SlideAction(
+                    decoration: BoxDecoration(color: Color(0xFF2675EC)),
+                    child: Icon(Icons.done_all, color: Colors.white, size: 22)),
+                SlideAction(
+                    decoration: BoxDecoration(color: Color(0xFF2675EC)),
+                    child: Icon(Icons.delete_outline,
+                        color: Colors.white, size: 22))
+              ],
             );
           }),
     );
@@ -312,5 +362,26 @@ class _MainScreen extends State<MainScreen> {
         children: [categoriesSlider(), Expanded(child: chats())],
       ),
     ));
+  }
+}
+
+class OnlineStatusCircle extends CustomPainter {
+  Paint _paint;
+
+  OnlineStatusCircle() {
+    _paint = Paint()
+      ..color = Color(0xFF2675EC)
+      ..strokeWidth = 1.0
+      ..style = PaintingStyle.fill;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawCircle(Offset(size.height / 2, size.width / 2), 7.0, _paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
